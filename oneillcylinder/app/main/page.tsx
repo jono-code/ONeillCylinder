2bc8266f7e9f4e38f0b7c7cf92d276b11f4a23a8
jono-code <jonofowkes@gmail.com>
2025-01-21 01:52:39 -0500

    Newist version

--- oneillcylinder/app/main/page.tsx
+++ oneillcylinder/app/main/page.tsx
@@ -5,14 +5,38 @@ export default function Page() {
      
     return (
     <div className='w-full h-full'>
-      <div className = 'flex-row'> </div>
-      <div></div>
-      <img src='/Cylinder.png'/>
-      <div className='relative w-11 h-11'> 
+      <div className='flex flex-row h-12 w-full border-b border-white object-top'>
+          <div className='w-4'/> 
+          <div> 
+          <div className='h-5'></div>
+           <img className='h-50 w-14' src="/CylWire.png"/>
+           </div>
+
+           <div className='w-5'></div>
+
+           <div> 
+           <div className='h-2'></div>
+           <p className='text-xl w-80' >O'NEILL_CYLINDERS an exploration</p>
+           </div> 
+           <div className='object-right w-4/6'> </div>
+           <img src='/faceNew.svg'/> 
+          </div>
+     
+
+      <div className='size-20'></div>
+      <img className='w-5/6 h-1/5' src='/Cylinder.png'/>
+      <div className='size-20'> 
+       </div>
+
+      <p className='text-xl w-80'></p>
+      <div className='flex flex-row '> 
+      <div className='relative w-20 h-20'> 
       <LinkImageCreate imglink ={'/origin.jpg'}
       tolink ={'https://www.w3schools.com/html/html_paragraphs.asp'}>
-      </LinkImageCreate>
+      </LinkImageCreate> 
       </div>
+      </div>
+
 
       <div className = ''> 
       <div className = ''> 
--- oneillcylinder/modules/textlink.tsx
+++ oneillcylinder/modules/textlink.tsx
@@ -5,9 +5,6 @@ export function LinkImageCreate({imglink, tolink}:{imglink:string, tolink:string
     return( 
             <div className='relative content-center'> 
                 <Link href = {tolink}> <img className='w-full h-auto' src = {imglink}/> </Link>
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <p> hello </p>
-                </div>
             </div>
     )
  
